7f59fc51319d0f7057723891ae2a68b353bf4ff0
malfell <malfellows13@gmail.com>
2022-05-05 20:42:56 -0500

    finished

--- public/app.js
+++ public/app.js
@@ -1,6 +1,14 @@
 // get user's data
 // get user's coordinates
+//need user's coordinates before displaying message--callback function--async and promise!
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
+// console.log(getCoords());
 
 // get user's time
 function userTime(){
@@ -8,7 +16,13 @@ function userTime(){
     return now.getHours();
 }
 
-console.log(userTime());
+function getMealTime(){
+    const tod = userTime();
+    //ternary operator 
+    return tod > 20 ? 'latenight snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
+
+// console.log(getMealTime());
 
 // helper functions
 // check time of day
@@ -16,10 +30,33 @@ console.log(userTime());
 
 // build ads
 // build ad 1
+function buildAd1(){
+    const mealTime = getMealTime();
+    let content = document.querySelector('.ad1');
+    let inner = document.createElement('p');
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`;
+    content.append(inner);
+}
+
+// buildAd1();
 
 
 // build ad 2
+function buildAd2(coordinates){
+    const coords = coordinates;
+    const href = `https://www.google.com/maps/search/coffee/@${coords[0]}, ${coords[1]},15z/`
+    let content = document.querySelector('.ad2');
+    let inner = document.createElement('p');
+    inner.innerHTML = `It's time to try our coffee! <span><a href"${href}" target="_blank">We're this close!</a></span>`;
+    content.append(inner)
+}
+// console.log(buildAd2(getCoords()))
 
 
 // event listeners
 // on load, build ads
+window.onload = async () => {
+    buildAd1();
+    const coords = await getCoords();
+    buildAd2(coords)
+}
